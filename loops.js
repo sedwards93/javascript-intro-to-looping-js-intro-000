266a90f4b9848a76828cf627fbc1f4fe84efedb0
Symion Edwards <symion.edwards@gmail.com>
2020-04-06 11:06:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -23,5 +23,5 @@ function incrementVariable(i) {
 
 function doWhileLoop (num) {
   do [console.log("I run once regardless.")];
-  while(incrementVariable() < 10)
+  while(incrementVariable() > 10)
 }
