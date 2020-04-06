1d6b62c1dae7e975acbb5aa8d1943d210c4caf73
Symion Edwards <symion.edwards@gmail.com>
2020-04-06 11:07:47 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,12 +16,9 @@ function whileLoop(n) {
   return "done";
 }
 
-function incrementVariable(i) {
-  i = i + 1;
-  return i;
-}
-
-function doWhileLoop (num) {
-  do [console.log("I run once regardless.")];
-  while(incrementVariable() > 10)
+function doWhileLoop(num) {
+  var i = 1;
+  do{
+    console.log("I run once regardless.");
+  }while(i++ < num);
 }
