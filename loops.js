93340671e78d348d65d7b4250718d12c08324cd8
Symion Edwards <symion.edwards@gmail.com>
2020-04-06 09:55:34 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,10 @@
 function forLoop(array) {
-  var = i;
-  for (let i = 0; i <25; i++);
-  return array.push( `I am ${i} strange loop${i===0 ?``:'s'}.`)
-}
+  for (let i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push("I am ${i} strange loops.");
+    }
+  }
+  return array;
